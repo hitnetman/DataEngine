4055e425a2d71ff04a9d70a6bc77c6caa7e01c97
hitnetman <78264231+hitnetman@users.noreply.github.com>
2021-02-01 03:53:27 -0600

    Add files via upload

---
+++ L1-1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Hello world!')
+
+
+# In[2]:
+
+
+
+# action 1
+
+sum=0
+
+for i in range(2,101,2):
+
+    sum=sum+i
+
+print (sum)
+
+
+# In[ ]:
+
+
+
+
